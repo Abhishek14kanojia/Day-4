60bea1acd60ab3dd87ad10831825ee607c058bc6
Abhishek Kanojia <abhishek14kanojia@gmail.com>
2022-11-27 18:50:55 +0530

    Day 4 Practice Problem

--- Employee_Wage.java
+++ Employee_Wage.java
@@ -4,16 +4,21 @@ package com.day4;
 public class Employee_Wage {
 
 	public static void main(String[] args) {
-		
-		int Full_Time = 1;
+		int Part_Time = 1;
+		int Full_Time = 2;
 		int Wage_Per_Hour = 20;
 		int EmpHrs = 0;
 		int EmpWage = 0;
 		
-		
-		double empCheck = Math.floor(Math.random()*10)%2;
-		
-		if(empCheck == Full_Time) {
+	
+		double empCheck = Math.floor(Math.random()*10)%3;
+		if (empCheck == Part_Time) {
+			System.out.println("Employee Present in Part Time");
+			EmpHrs = 4;
+			EmpWage = EmpHrs * Wage_Per_Hour;
+			System.out.println("EmpWage is : " + EmpWage);
+			
+		}else if(empCheck == Full_Time) {
 			System.out.println("Employee is Present");
 			EmpHrs = 8;
 			EmpWage = EmpHrs * Wage_Per_Hour;
