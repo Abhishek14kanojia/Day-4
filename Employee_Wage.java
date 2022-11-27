a9846fecdd2f08d390e93551cf5200270fd906be
Abhishek Kanojia <abhishek14kanojia@gmail.com>
2022-11-27 20:21:03 +0530

    Day 4 Practice Problem

--- Employee_Wage.java
+++ Employee_Wage.java
@@ -7,14 +7,18 @@ public class Employee_Wage {
 	public static final int Full_Time = 2;
 	public static final int Wage_Per_Hour = 20;
 	public static final int Monthly_Working_days = 20;
-
+	public static final int Hrs_in_Month = 100;
+	
 	public static void main(String[] args) {
 		
 		int EmpHrs = 0;
 		int EmpWage = 0;
 		int TotalWage = 0;
+		int totalEmpHrs = 0;
+		int TotalWorkingDays = 0;
 		
-		for(int day = 0; day < Monthly_Working_days; day++) {
+		while(totalEmpHrs <= Hrs_in_Month && TotalWorkingDays < Monthly_Working_days) {
+			TotalWorkingDays++;
 		int empCheck = (int) (Math.floor(Math.random()*10)%3);
 		
 		switch (empCheck) {
@@ -23,9 +27,10 @@ public class Employee_Wage {
 			System.out.println("Employee Present in Part Time :");
 			EmpWage = EmpHrs * Wage_Per_Hour;
 			System.out.println("Part time wage is : " + EmpWage);
-			TotalWage = TotalWage + EmpWage;
-			System.out.println("Total wage is : " + TotalWage);
-		
+			TotalWage = totalEmpHrs * Wage_Per_Hour;
+			System.out.println("Total Emp Wage : " + TotalWage);
+			totalEmpHrs = totalEmpHrs + EmpHrs;
+			System.out.println("Day# : " + TotalWorkingDays + " Hours : " + EmpHrs);
 			break;
 			
 		case Full_Time:
@@ -33,9 +38,10 @@ public class Employee_Wage {
 			System.out.println("Employee Present in Full Time :");
 			EmpWage = EmpHrs * Wage_Per_Hour;
 			System.out.println("Full time wage is : " + EmpWage);
-			TotalWage = TotalWage + EmpWage;
-			System.out.println("Total wage is : " + TotalWage);
-		
+			TotalWage = totalEmpHrs * Wage_Per_Hour;
+			System.out.println("Total Emp Wage" + TotalWage);
+			totalEmpHrs = totalEmpHrs + EmpHrs;
+			System.out.println("Day# : " + TotalWorkingDays + " Hours : " + EmpHrs);
 			break;
 			
 		default:
@@ -43,13 +49,13 @@ public class Employee_Wage {
 			System.out.println("Employee not Present in Full Time :");
 			EmpWage = EmpHrs * Wage_Per_Hour;
 			System.out.println("Employee not Present : " + EmpWage);
-			TotalWage = TotalWage + EmpWage;
-			System.out.println("Total wage is : " + TotalWage);
-		
+			TotalWage = totalEmpHrs * Wage_Per_Hour;
+			System.out.println("Total emp Wage : " + TotalWage);
+			totalEmpHrs = totalEmpHrs + EmpHrs;
+			System.out.println("Day# : " + TotalWorkingDays + " Hours : " + EmpHrs);
 		}
 		
-		}
-		
-		}
+		} 
+	}
 	
 }
