9d1e460c696b672a3e422a7605494cdaec195f7d
Abhishek Kanojia <abhishek14kanojia@gmail.com>
2022-11-27 19:41:30 +0530

    Day 4 Practice Problem

--- Employee_Wage.java
+++ Employee_Wage.java
@@ -6,13 +6,15 @@ public class Employee_Wage {
 	public static final int  Part_Time = 1;
 	public static final int Full_Time = 2;
 	public static final int Wage_Per_Hour = 20;
-	
+	public static final int Monthly_Working_days = 20;
+
 	public static void main(String[] args) {
 		
 		int EmpHrs = 0;
 		int EmpWage = 0;
+		int TotalWage = 0;
 		
-	
+		for(int day = 0; day < Monthly_Working_days; day++) {
 		int empCheck = (int) (Math.floor(Math.random()*10)%3);
 		
 		switch (empCheck) {
@@ -21,6 +23,9 @@ public class Employee_Wage {
 			System.out.println("Employee Present in Part Time :");
 			EmpWage = EmpHrs * Wage_Per_Hour;
 			System.out.println("Part time wage is : " + EmpWage);
+			TotalWage = TotalWage + EmpWage;
+			System.out.println("Total wage is : " + TotalWage);
+		
 			break;
 			
 		case Full_Time:
@@ -28,6 +33,9 @@ public class Employee_Wage {
 			System.out.println("Employee Present in Full Time :");
 			EmpWage = EmpHrs * Wage_Per_Hour;
 			System.out.println("Full time wage is : " + EmpWage);
+			TotalWage = TotalWage + EmpWage;
+			System.out.println("Total wage is : " + TotalWage);
+		
 			break;
 			
 		default:
@@ -35,11 +43,13 @@ public class Employee_Wage {
 			System.out.println("Employee not Present in Full Time :");
 			EmpWage = EmpHrs * Wage_Per_Hour;
 			System.out.println("Employee not Present : " + EmpWage);
-	
+			TotalWage = TotalWage + EmpWage;
+			System.out.println("Total wage is : " + TotalWage);
+		
 		}
 		
+		}
 		
-		
-	}
+		}
 	
 }
